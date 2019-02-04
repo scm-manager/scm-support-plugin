a514775df8b23de51d97b21c96e285b5beedb084
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-02-04 09:47:54 +0100

    Suppress warning

--- src/main/java/sonia/scm/support/LoggingStateDto.java
+++ src/main/java/sonia/scm/support/LoggingStateDto.java
@@ -5,6 +5,7 @@ import de.otto.edison.hal.Links;
 import lombok.Getter;
 
 @Getter
+@SuppressWarnings("squid:S2160") // There is no need and no semantics for equals here
 class LoggingStateDto extends HalRepresentation {
   LoggingStateDto(boolean logRunning, boolean processingLog, Links links) {
     super(links);
