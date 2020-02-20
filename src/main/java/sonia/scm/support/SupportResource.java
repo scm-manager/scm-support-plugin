30865427cff164130628ff2e4c5074ecf557eca4
Florian Scholdei <florian.scholdei@cloudogu.com>
2020-02-20 15:24:22 +0100

    Add REST doc

--- src/main/java/sonia/scm/support/SupportResource.java
+++ src/main/java/sonia/scm/support/SupportResource.java
@@ -1,9 +1,9 @@
 /**
  * Copyright (c) 2010, Sebastian Sdorra All rights reserved.
- *
+ * <p>
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *
+ * <p>
  * 1. Redistributions of source code must retain the above copyright notice,
  * this list of conditions and the following disclaimer. 2. Redistributions in
  * binary form must reproduce the above copyright notice, this list of
@@ -11,7 +11,7 @@
  * materials provided with the distribution. 3. Neither the name of SCM-Manager;
  * nor the names of its contributors may be used to endorse or promote products
  * derived from this software without specific prior written permission.
- *
+ * <p>
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -22,24 +22,28 @@
  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * <p>
  * http://bitbucket.org/sdorra/scm-manager
- *
  */
 
-
-
 package sonia.scm.support;
-
 
 import com.google.common.io.ByteStreams;
 import com.google.common.io.Closeables;
 import com.google.inject.Inject;
 import de.otto.edison.hal.Link;
 import de.otto.edison.hal.Links;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sonia.scm.api.v2.resources.ErrorDto;
 import sonia.scm.store.Blob;
+import sonia.scm.web.VndMediaType;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -54,20 +58,19 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
- *
  * @author Sebastian Sdorra
  */
+@OpenAPIDefinition(tags = {
+  @Tag(name = "Support Plugin", description = "Support plugin provided endpoints")
+})
 @Path("v2/plugins/support")
-public class SupportResource
-{
+public class SupportResource {
 
   private static final Logger log = LoggerFactory.getLogger(SupportResource.class);
-
   private static final String MEDIA_TYPE_ZIP = "application/zip";
 
   @Inject
-  public SupportResource(SupportManager supportManager, SupportLinks links)
-  {
+  public SupportResource(SupportManager supportManager, SupportLinks links) {
     this.supportManager = supportManager;
     this.links = links;
   }
@@ -75,8 +78,25 @@ public class SupportResource
   @GET
   @Path("")
   @Produces(MEDIA_TYPE_ZIP)
-  public Response createSupportFile()
-  {
+  @Operation(summary = "Collect and download data", description = "Collects data and downloads it as zip.", tags = "Support Plugin")
+  @ApiResponse(
+    responseCode = "200",
+    description = "success",
+    content = @Content(
+      mediaType = MEDIA_TYPE_ZIP
+    )
+  )
+  @ApiResponse(responseCode = "401", description = "not authenticated / invalid credentials")
+  @ApiResponse(responseCode = "403", description = "not authorized, the current user does not have the \"support:information\" privilege")
+  @ApiResponse(
+    responseCode = "500",
+    description = "internal server error",
+    content = @Content(
+      mediaType = VndMediaType.ERROR_TYPE,
+      schema = @Schema(implementation = ErrorDto.class)
+    )
+  )
+  public Response createSupportFile() {
     SupportPermissions.checkReadInformation();
     try {
       return createBlobResponse(supportManager.collectSupportData());
@@ -88,8 +108,25 @@ public class SupportResource
   @GET
   @Path("logging/disable")
   @Produces(MEDIA_TYPE_ZIP)
-  public Response disableTraceLogging()
-  {
+  @Operation(summary = "Disable and download logging data", description = "Disable logging and download of collected support data as zip.", tags = "Support Plugin")
+  @ApiResponse(
+    responseCode = "200",
+    description = "success",
+    content = @Content(
+      mediaType = MEDIA_TYPE_ZIP
+    )
+  )
+  @ApiResponse(responseCode = "401", description = "not authenticated / invalid credentials")
+  @ApiResponse(responseCode = "403", description = "not authorized, the current user does not have the \"support:information\" privilege")
+  @ApiResponse(
+    responseCode = "500",
+    description = "internal server error",
+    content = @Content(
+      mediaType = VndMediaType.ERROR_TYPE,
+      schema = @Schema(implementation = ErrorDto.class)
+    )
+  )
+  public Response disableTraceLogging() {
     SupportPermissions.checkStartLog();
     log.info("disable trace log");
     try {
@@ -101,8 +138,19 @@ public class SupportResource
 
   @POST
   @Path("logging/enable")
-  public Response enableTraceLogging() throws IOException
-  {
+  @Operation(summary = "Activate logging", description = "Activates the logging for collecting support data.", tags = "Support Plugin")
+  @ApiResponse(responseCode = "204", description = "no content")
+  @ApiResponse(responseCode = "401", description = "not authenticated / invalid credentials")
+  @ApiResponse(responseCode = "403", description = "not authorized, the current user does not have the \"support:logging\" privilege")
+  @ApiResponse(
+    responseCode = "500",
+    description = "internal server error",
+    content = @Content(
+      mediaType = VndMediaType.ERROR_TYPE,
+      schema = @Schema(implementation = ErrorDto.class)
+    )
+  )
+  public Response enableTraceLogging() throws IOException {
     SupportPermissions.checkStartLog();
     log.info("enable trace log");
     supportManager.enableTraceLogging();
@@ -112,8 +160,26 @@ public class SupportResource
   @GET
   @Path("logging")
   @Produces(MediaType.APPLICATION_JSON)
-  public Response loggingState()
-  {
+  @Operation(summary = "Get logging status", description = "Gets the logging status for collecting support data.", tags = "Support Plugin")
+  @ApiResponse(
+    responseCode = "200",
+    description = "success",
+    content = @Content(
+      mediaType = MediaType.APPLICATION_JSON,
+      schema = @Schema(implementation = LoggingStateDto.class)
+    )
+  )
+  @ApiResponse(responseCode = "401", description = "not authenticated / invalid credentials")
+  @ApiResponse(responseCode = "403", description = "not authorized, the current user does not have the \"support:information\" privilege")
+  @ApiResponse(
+    responseCode = "500",
+    description = "internal server error",
+    content = @Content(
+      mediaType = VndMediaType.ERROR_TYPE,
+      schema = @Schema(implementation = ErrorDto.class)
+    )
+  )
+  public Response loggingState() {
     SupportPermissions.checkStartLog();
     Links.Builder links = Links.linkingTo()
       .self(this.links.createLogStatusLink());
@@ -129,27 +195,24 @@ public class SupportResource
       .build();
   }
 
-  private Response createBlobResponse(Blob blob)
-  {
+  private Response createBlobResponse(Blob blob) {
     //J-
     return Response.ok(
       new BlobStreamingOutput(blob)
     )
-    .header(
-      "Content-Disposition",
-      "attachment; filename=\"".concat(blob.getId()).concat(".zip\"")
-    )
-    .build();
+      .header(
+        "Content-Disposition",
+        "attachment; filename=\"".concat(blob.getId()).concat(".zip\"")
+      )
+      .build();
     //J+
   }
 
   //~--- inner classes --------------------------------------------------------
 
-  public static class BlobStreamingOutput implements StreamingOutput
-  {
+  public static class BlobStreamingOutput implements StreamingOutput {
 
-    public BlobStreamingOutput(Blob blob)
-    {
+    public BlobStreamingOutput(Blob blob) {
       this.blob = blob;
     }
 
@@ -157,17 +220,13 @@ public class SupportResource
 
     @Override
     public void write(OutputStream output)
-      throws IOException, WebApplicationException
-    {
+      throws IOException, WebApplicationException {
       InputStream input = null;
 
-      try
-      {
+      try {
         input = blob.getInputStream();
         ByteStreams.copy(input, output);
-      }
-      finally
-      {
+      } finally {
         Closeables.close(input, true);
       }
     }
