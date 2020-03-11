b901ea034c191c78a5efb2c2caef13475c1a86ea
René Pfeuffer <rene@pfeuffer-web.de>
2020-03-11 11:47:46 +0100

    Make navigation item collapsable

--- src/main/js/SupportNavLink.tsx
+++ src/main/js/SupportNavLink.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { WithTranslation, withTranslation } from "react-i18next";
-import { NavLink } from "@scm-manager/ui-components";
+import { SecondaryNavigationItem } from "@scm-manager/ui-components";
 
 type Props = WithTranslation;
 
@@ -8,7 +8,14 @@ class SupportNavLink extends React.Component<Props> {
   render() {
     const { t } = this.props;
 
-    return <NavLink to="/admin/support" icon="fas fa-life-ring" label={t("scm-support-plugin.navLink")} />;
+    return (
+      <SecondaryNavigationItem
+        to={`/admin/support`}
+        icon="fas fa-life-ring"
+        label={t("scm-support-plugin.navLink")}
+        title={t("scm-support-plugin.navLink")}
+      />
+    );
   }
 }
 
